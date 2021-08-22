f54aba0e9025c33a41e198c14ef2e84eb322cd24
AshikaChadaga <ashikachadaga@gmail.com>
2021-08-22 12:44:38 +0530

    ADDED: Check Vowel Or Consonant Program

---
+++ src/com/bridgelabz/basiccoreprograms/VowelOrConsonant.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.basiccoreprograms;
+
+public class VowelOrConsonant {
+
+	public static void main(String[] args) {
+
+		char alphabet = 'V';
+
+		if (alphabet == 'a' || alphabet == 'e' || alphabet == 'i' || alphabet == 'o' || alphabet == 'u'
+				|| alphabet == 'A' || alphabet == 'E' || alphabet == 'I' || alphabet == 'O' || alphabet == 'U') {
+			System.out.println(alphabet + " is a VOWEL");
+		} else {
+			System.out.println(alphabet + " is a CONSONANT");
+		}
+
+	}
+
+}
